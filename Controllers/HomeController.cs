e72df50fc96cab66d6b4e31ce70ba884f70b1865
Mwika Kamina Ntenda <u20551101@up.ac.za>
2023-12-11 20:23:33 +0200

    Creating table

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Task_Managemenet.Models;
 
 namespace Task_Managemenet.Controllers
 {
     public class HomeController : Controller
     {
+        // Public property for the context
+        private TaskManagementEntities db = new TaskManagementEntities();
+
         public ActionResult Index()
         {
+            // Fetch priorities and status types from the database
+            ViewBag.Priorities = db.Priority.Select(p => new SelectListItem
+            {
+                Value = p.PriorityID.ToString(),
+                Text = p.PriorityName
+            });
+
+            ViewBag.StatusTypes = db.Status.Select(s => new SelectListItem
+            {
+                Value = s.StatusID.ToString(),
+                Text = s.StatusType
+            });
+
+            // Fetch tasks from the database
+            ViewBag.Tasks = db.Task.ToList();
+
             return View();
         }
 
